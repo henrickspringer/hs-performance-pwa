eb0def51e194d3144ae38daf9305ea6e85aacc4b
henrickspringer <henrickspringer.nutri@gmail.com>
2025-07-30 22:56:19 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+
+self.addEventListener('install', function (event) {
+  console.log('Service Worker instalado');
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(fetch(event.request));
+});
